229784429c7ad7fe9f83ab80f27ee89aa1cd7d1d
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-06-27 00:08:35 -0300

    GET - data by id, mongoId o por nombre

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -21,7 +21,7 @@ export class PokemonController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.pokemonService.findOne(+id);
+    return this.pokemonService.findOne(id);
   }
 
   @Patch(':id')
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,7 +1,7 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 
@@ -37,8 +37,26 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(id: string) {
+    let pokemon: Pokemon;
+
+    if(!isNaN(+id)) {
+      pokemon = await this.pokemonModel.findOne({no: id});
+    }
+
+    //MongoId verificacion
+    if( !pokemon && isValidObjectId(id)){
+      pokemon = await this.pokemonModel.findById(id);
+    }
+
+    //Name
+    if(!pokemon){
+      pokemon = await this.pokemonModel.findOne({name: id.toLowerCase().trim()});
+    }
+
+    if(!pokemon) throw new NotFoundException('Pokemon not found');
+
+    return pokemon;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
