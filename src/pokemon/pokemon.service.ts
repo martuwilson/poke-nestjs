2a8c78d7e718e43d54bafe9e708503f382b4156e
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-06-26 23:31:30 -0300

    POST method - insertar data en la base de datos

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ){}
+
+
+  async create(createPokemonDto: CreatePokemonDto) {
 
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
 
-    return createPokemonDto
+    //Inserción de datos en la base de datos
+    const pokemon = await this.pokemonModel.create(createPokemonDto);
+
+    return pokemon;
   }
 
   findAll() {
